6be5df64a7cc9694e330059b7fc74fdc275480fc
Zyad <zyadgohary@hotmail.com>
2019-09-10 16:05:35 -0700

    Fixed Issue #33

--- Dev/config/screenStyle.js
+++ Dev/config/screenStyle.js
@@ -1,11 +1,13 @@
 //The style that will be used for all screens through out the app to fix the flow
 import colors from './colors';
+import { Dimensions } from 'react-native';
 
 export default {
     container: {
         flexDirection: 'column',
         alignItems: 'center',
         backgroundColor: colors.lightGrey,
-        flex: 1
+        flex: 1,
+        width: Dimensions.get('window').width
     }
 }
