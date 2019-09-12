9af50047c795f3433e20494c78c1aef62af7a1fc
zyadelgohary <zyadelgohary@users.noreply.github.com>
2019-09-11 23:24:57 -0700

    Fixed Issue #49

--- Dev/src/components/FlowLayout.js
+++ Dev/src/components/FlowLayout.js
@@ -342,7 +342,7 @@ const styles = StyleSheet.create({
 		borderColor: colors.grey,
 		borderWidth: 1 / PixelRatio.get(),
 		borderRadius: 5,
-		height: 35,
+		height: Dimensions.get('window').height * 0.05,
 		justifyContent: 'center',
 		alignItems: 'center',
 		paddingLeft: 10,
