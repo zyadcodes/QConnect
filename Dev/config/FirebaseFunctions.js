2382e4b7702505be0cae00f0a579db680d8b438b
Elyasse <elyassedev@outlook.com>
2019-10-07 12:06:53 -0700

    Revert "Revert "Merge branch 'app-release' into quran-component""
    This reverts commit 29bf1eb7f1c36d037d9a3d485283cf524b041771.

--- Dev/config/FirebaseFunctions.js
+++ Dev/config/FirebaseFunctions.js
@@ -444,6 +444,14 @@ export default class FirebaseFunctions {
         });
         this.logEvent("JOIN_CLASS");
 
+        //Sends a notification to the teachers of that class saying that a student has joined the class
+        classToJoin.data().teachers.forEach((teacherID) => {
+            this.functions.httpsCallable('sendNotification', {
+                topic: teacherID,
+                title: strings.NewStudent,
+                body: student.name + strings.HasJoinedYourClass
+            })
+        })
         return studentObject;
 
     }
