c4a7784cee8b3131f51156229819e70262e96a9f
Zyad <zyadgohary@hotmail.com>
2019-10-11 18:11:27 -0700

    Fixed Issue #189

--- Dev/App.js
+++ Dev/App.js
@@ -1,14 +1,42 @@
 import React, { Component } from 'react';
 import MainStackNavigator from './src/screens/MainStackNavigator';
 import { YellowBox } from 'react-native';
+import QCView from 'components/QCView';
+import screenStyle from 'config/screenStyle';
+import { checkNotifications, requestNotifications, check, request, PERMISSIONS } from 'react-native-permissions';
 
 export default class App extends Component {
 
+  state = {
+    requestingPermissions: true,
+  }
+
+  async componentDidMount() {
+
+    const isPermissionsGranted = await checkNotifications();
+    if (isPermissionsGranted !== 'granted') {
+      await requestNotifications();
+      this.setState({ requestingPermissions: false });
+    } else {
+      this.setState({ requestingPermissions: false });
+    }
+
+  }
+
   render() {
+
     YellowBox.ignoreWarnings(['componentWillUpdate', 'componentWillMount', 'componentWillReceiveProps']);
-    return (
-      <MainStackNavigator />
-    );
+
+    if (this.state.requestingPermissions === false) {
+      return (
+        <MainStackNavigator />
+      );
+    } else  {
+      return (
+        <QCView style={screenStyle.container} />
+      )
+    }
+
   }
 
 }
