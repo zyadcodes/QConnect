d97dc494d4ef36700b689ab39881d056caaa538a
Elyasse <elyassedev@outlook.com>
2020-01-31 13:41:11 -0800

    Removed BarChart since it is not yet functional

--- Dev/src/screens/TeacherScreens/ClassTabs/StudentProfileScreen.js
+++ Dev/src/screens/TeacherScreens/ClassTabs/StudentProfileScreen.js
@@ -67,6 +67,53 @@ class StudentProfileScreen extends QcParentScreen {
     });
   }
 
+  drawBarChart() {
+    return (
+      <BarChart
+        data={{
+          labels: ['January', 'February', 'March', 'April', 'May', 'June'],
+          datasets: [
+            {
+              data: [
+                Math.random() * 100,
+                Math.random() * 100,
+                Math.random() * 100,
+                Math.random() * 100,
+                Math.random() * 100,
+                Math.random() * 100
+              ]
+            }
+          ]
+        }}
+        width={screenWidth}
+        height={220}
+        yAxisLabel="$"
+        yAxisSuffix="k"
+        chartConfig={{
+          backgroundColor: '#e26a00',
+          backgroundGradientFrom: '#fb8c00',
+          backgroundGradientTo: '#ffa726',
+          decimalPlaces: 2, // optional, defaults to 2dp
+          color: (opacity = 1) => `rgba(255, 255, 255, ${opacity})`,
+          labelColor: (opacity = 1) => `rgba(255, 255, 255, ${opacity})`,
+          style: {
+            borderRadius: 16
+          },
+          propsForDots: {
+            r: '6',
+            strokeWidth: '2',
+            stroke: '#ffa726'
+          }
+        }}
+        bezier
+        style={{
+          marginVertical: 8,
+          borderRadius: 16
+        }}
+      />
+    );
+  }
+
   setDialogueVisible(visible) {
     this.setState({ isDialogVisible: visible });
   }
@@ -244,48 +291,6 @@ class StudentProfileScreen extends QcParentScreen {
           ) : (
             <View />
           )}
-          <BarChart
-            data={{
-              labels: ['January', 'February', 'March', 'April', 'May', 'June'],
-              datasets: [
-                {
-                  data: [
-                    Math.random() * 100,
-                    Math.random() * 100,
-                    Math.random() * 100,
-                    Math.random() * 100,
-                    Math.random() * 100,
-                    Math.random() * 100
-                  ]
-                }
-              ]
-            }}
-            width={screenWidth}
-            height={220}
-            yAxisLabel="$"
-            yAxisSuffix="k"
-            chartConfig={{
-              backgroundColor: '#e26a00',
-              backgroundGradientFrom: '#fb8c00',
-              backgroundGradientTo: '#ffa726',
-              decimalPlaces: 2, // optional, defaults to 2dp
-              color: (opacity = 1) => `rgba(255, 255, 255, ${opacity})`,
-              labelColor: (opacity = 1) => `rgba(255, 255, 255, ${opacity})`,
-              style: {
-                borderRadius: 16
-              },
-              propsForDots: {
-                r: '6',
-                strokeWidth: '2',
-                stroke: '#ffa726'
-              }
-            }}
-            bezier
-            style={{
-              marginVertical: 8,
-              borderRadius: 16
-            }}
-          />
           <FlatList
             data={this.state.classStudent.currentAssignments}
             keyExtractor={(item, index) => item.name + index}
