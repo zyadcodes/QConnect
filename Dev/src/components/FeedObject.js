c8f501c650208518716368ae60f9763bafa0dcec
yoyo7373 <yousefgomaa@hotmail.com>
2020-05-11 18:50:05 -0700

    I added a lot of things today, and I reformatted code using eslint, I specifically fixed up something small with the Quran view and then added the reactions ability, but I still haven't implemented this in Firebase, as I am still thinking about how I'll organize it

--- Dev/src/components/FeedObject.js
+++ Dev/src/components/FeedObject.js
@@ -1,156 +1,294 @@
-import React, {Component} from 'react'
+import React, { Component } from 'react';
 import {
-    View,
-    TouchableOpacity,
-    Text,
-    Image,
-    StyleSheet,
-    Dimensions
-} from 'react-native'
-import {screenHeight, screenWidth, screenScale, fontScale} from '../../config/dimensions'
-import PropTypes from 'prop-types'
-import colors from '../../config/colors'
-import {getPageTextWbW} from '../screens/MushafScreen/ServiceActions/getQuranContent'
-import SurahHeader from '../screens/MushafScreen/Components/SurahHeader'
-import Basmalah from '../screens/MushafScreen/Components/Basmalah'
-import TextLine from '../screens/MushafScreen/Components/TextLine'
-import LoadingSpinner from './LoadingSpinner'
+  View,
+  TouchableOpacity,
+  Text,
+  FlatList,
+  Image,
+  StyleSheet,
+} from 'react-native';
+import {
+  screenHeight,
+  screenWidth,
+  screenScale,
+  fontScale,
+} from '../../config/dimensions';
+import PropTypes from 'prop-types';
+import colors from '../../config/colors';
+import { getPageTextWbW } from '../screens/MushafScreen/ServiceActions/getQuranContent';
+import SurahHeader from '../screens/MushafScreen/Components/SurahHeader';
+import Basmalah from '../screens/MushafScreen/Components/Basmalah';
+import TextLine from '../screens/MushafScreen/Components/TextLine';
+import LoadingSpinner from './LoadingSpinner';
+import { Icon } from 'react-native-elements';
+import Modal from 'react-native-modal';
 
-export default class FeedsObject extends Component{
-    static propTypes = {
-        type: PropTypes.string.isRequired,
-        Content: PropTypes.object.isRequired,
-        userID: PropTypes.string.isRequired,
-        reactions: PropTypes.array.isRequired,
-        imageRequire: PropTypes.object.isRequired,
-    }
-    state = {
-        surahName: '',
-        page: [],
-        isLoading: false,
-    }
-    async componentDidMount(){
-        console.log(screenHeight)
-        if(this.props.type === 'assignment'){
-            this.setState({isLoading: true})
-            const pageLines = await getPageTextWbW(this.props.Content.start.page);
-            let allAyat = (
-                    <View>
-                        {pageLines !== undefined &&
-                        pageLines.map((line, index) => {
-                            if (line.type === 'besmellah') {
-                                return <Basmalah key={line.line + "_basmalah"} />;
-                            } else if(line.type !== 'start_sura') {
-                            return (
-                                <TextLine
-                                key={this.props.Content.start.page + '_' + line.line}
-                                lineText={line.text}
-                                selectionOn={false}
-                                highlightedWord={undefined}
-                                selectedAyahsEnd={this.props.Content.end}
-                                selectedAyahsStart={this.props.Content.start}
-                                selectionStarted={false}
-                                selectionCompleted={false}
-                                isFirstWord={false}
-                                onSelectAyah={(ayah, word) => {}}
-                                page={this.props.Content.start.page}
-                                lineAlign={(this.props.Content.start.page === 1 ? 'center' : 'stretch')}
-                                />
-                            );
-                            }
-                        })}
-                    </View>
-            )
-            this.setState({surahName: pageLines[0].surah, page: allAyat, isLoading: false})
-        }
-    }
-    render(){
-        return (
-            <View style={this.localStyles.containerView}>
-                <Image source={this.props.imageRequire} style={this.localStyles.userImage}/>
-                <View style={{flex: 5, marginTop: screenScale*18}}>
-                    {this.props.type === 'assignment' ? 
-                        <Text style={[this.localStyles.newAssignmentText, {fontSize: fontScale*18}]}>New Assignment: </Text>
-                        :
-                        null
+export default class FeedsObject extends Component {
+  static propTypes = {
+    type: PropTypes.string.isRequired,
+    Content: PropTypes.object.isRequired,
+    userID: PropTypes.string.isRequired,
+    reactions: PropTypes.array.isRequired,
+    imageRequire: PropTypes.object.isRequired,
+    onPressSelectEmoji: PropTypes.func.isRequired
+  };
+  state = {
+    surahName: '',
+    page: [],
+    isLoading: false,
+  };
+  async componentDidMount() {
+    console.log(screenHeight);
+    if (this.props.type === 'assignment') {
+      this.setState({ isLoading: true });
+      const pageLines = await getPageTextWbW(this.props.Content.start.page);
+      let allAyat = (
+        <View>
+          {pageLines !== undefined &&
+            pageLines.map((line, index) => {
+              if (
+                line.type === 'besmellah' &&
+                this.props.Content.start.ayah === 1
+              ) {
+                return <Basmalah key={line.line + "_basmalah"} />;
+              } else if (
+                line.ayah >= this.props.Content.start.ayah &&
+                line.ayah <= this.props.Content.end.ayah
+              ) {
+                return (
+                  <TextLine
+                    key={this.props.Content.start.page + '_' + line.line}
+                    lineText={line.text}
+                    selectionOn={false}
+                    highlightedWord={undefined}
+                    selectedAyahsEnd={this.props.Content.end}
+                    selectedAyahsStart={this.props.Content.start}
+                    selectionStarted={false}
+                    selectionCompleted={false}
+                    isFirstWord={false}
+                    onSelectAyah={(ayah, word) => {}}
+                    page={this.props.Content.start.page}
+                    lineAlign={
+                      this.props.Content.start.page === 1 ? 'center' : 'stretch'
                     }
-                    <View style={this.localStyles.contentContainerView}>
-                        {this.props.type === 'assignment' ? 
-                                <View>
-                                    <Text style={[this.localStyles.newAssignmentText, {marginLeft: screenWidth/86}]}>{this.props.Content.assignmentType} from ayah {this.props.Content.start.ayah} to ayah {this.props.Content.end.ayah}</Text>
-                                    <TouchableOpacity disabled={this.props.isTeacher} style={this.localStyles.assignmentContainer}>
-                                        {this.state.isLoading ?
-                                            <View style={this.localStyles.spinnerContainerStyle}>
-                                                <LoadingSpinner/>
-                                            </View>
-                                            :   
-                                            <View style={{flex: 1}}> 
-                                                <SurahHeader surahName={this.state.surahName}/>
-                                                {this.state.page}
-                                            </View>
-                                        }
-                                    </TouchableOpacity>
-                                    <Text style={[this.localStyles.newAssignmentText, {alignSelf: 'flex-end', marginRight: screenWidth/86}]}>Click To Open</Text>
-                                </View>
-                            :
-                            <Text style={this.localStyles.contentText}>{this.props.Content}</Text>
-                        }
-                    </View>
-                </View>
-            </View>
-        )
+                  />
+                );
+              }
+            })}
+        </View>
+      );
+      this.setState({
+        surahName: pageLines[0].surah,
+        page: allAyat,
+        isLoading: false,
+      });
     }
-    localStyles = StyleSheet.create({
-        containerView: {
-            width: (this.props.type == 'assignment' ? 2.4*screenWidth/3 : 2*screenWidth/3),
-            height: (this.props.type == 'assignment' ? 
-                    screenHeight/3 : screenHeight/7),
-            flexDirection: 'row',
-            marginTop: (this.props.number == 0 ? screenHeight/40 : 0),
-            marginBottom: screenHeight/40
-        },
-        assignmentContainer: {
-            alignSelf: 'center',
-            flex: 3,
-            overflow: 'hidden',
-            borderColor: colors.lightBrown,
-            borderWidth: 2,
-            marginTop: screenHeight/163.2,
-            width: screenWidth/1.6
-        },
-        spinnerContainerStyle: {
-            flex: 1,
-            alignItems: 'center',
-            justifyContent: 'center'
-        },
-        contentText: {
-            color: colors.lightBrown,
-            fontWeight: 'bold',
-            fontSize: fontScale*15
-        },
-        userImage: {
-            width: screenScale*18,
-            height: screenScale*18,
-            marginLeft: screenWidth/25,
-            resizeMode: 'contain',
-        },
-        newAssignmentText: {
-            fontSize: fontScale*16,
-            color: colors.lightBrown,
-            fontWeight: 'bold'
-        },
-        newAssignmentTextContainer: {
-            flexDirection: 'row'
-        },
-        contentContainerView: {
-            borderWidth: 2,
-            marginTop: (this.props.type === 'assignment' ? screenHeight/50 : 0),
-            flex: 5,
-            paddingBottom: (this.props.type === 'assignment' ? screenHeight/163.5 : 0),
-            alignItems: 'center',
-            justifyContent: 'center',
-            borderColor: colors.lightBrown,
-            backgroundColor: colors.white,
-        }
-    })
+  }
+  render() {
+    return (
+      <View style={this.localStyles.containerView}>
+        <Image
+          source={this.props.imageRequire}
+          style={this.localStyles.userImage}
+        />
+        <View style={{ flex: 5, marginTop: screenScale * 18 }}>
+          {this.props.type === 'assignment' ? (
+            <Text
+              style={[
+                this.localStyles.newAssignmentText,
+                { fontSize: fontScale * 18 },
+              ]}
+            >
+              New Assignment:{' '}
+            </Text>
+          ) : null}
+          <View style={this.localStyles.contentContainerView}>
+            {this.props.type === 'assignment' ? (
+              <View>
+                <Text
+                  style={[
+                    this.localStyles.newAssignmentText,
+                    {
+                      marginLeft:
+                        this.props.madeByUser == this.props.currentUser.ID
+                          ? 0
+                          : screenWidth / 86,
+                      marginRight:
+                        this.props.madeByUser == this.props.currentUser.ID
+                          ? screenWidth / 86
+                          : 0,
+                    },
+                  ]}
+                >
+                  {this.props.Content.assignmentType} from ayah{' '}
+                  {this.props.Content.start.ayah} to ayah{' '}
+                  {this.props.Content.end.ayah}
+                </Text>
+                {this.state.isLoading ? (
+                  <TouchableOpacity
+                    disabled={this.props.isTeacher}
+                    style={this.localStyles.assignmentContainer}
+                  >
+                    <View style={this.localStyles.spinnerContainerStyle}>
+                      <LoadingSpinner />
+                    </View>
+                  </TouchableOpacity>
+                ) : (
+                  <TouchableOpacity
+                    disabled={this.props.isTeacher}
+                    style={this.localStyles.assignmentContainer}
+                  >
+                    <SurahHeader surahName={this.state.surahName} />
+                    {this.state.page}
+                  </TouchableOpacity>
+                )}
+                {this.props.isTeacher ? null : (
+                  <Text
+                    style={[
+                      this.localStyles.newAssignmentText,
+                      { alignSelf: 'flex-end', marginRight: screenWidth / 86 },
+                    ]}
+                  >
+                    Click To Open
+                  </Text>
+                )}
+              </View>
+            ) : (
+              <Text style={this.localStyles.contentText}>
+                {this.props.Content}
+              </Text>
+            )}
+          </View>
+          <View style={{ flexDirection: 'row', alignSelf: 'flex-end' }}>
+            <FlatList
+              data={this.props.Reactions}
+              renderItem={({ item, index, seperators }) => (
+                <TouchableOpacity
+                  onPress={() => {}}
+                  style={this.localStyles.Reaction}
+                >
+                  <Text>{item.reactedBy.length}</Text>
+                  <Text>{item.emoji}</Text>
+                </TouchableOpacity>
+              )}
+            />
+            {this.props.madeByUser == this.props.currentUser.ID ? null : (
+              <TouchableOpacity
+                onPress={() => this.props.onPressSelectEmoji()}
+                style={this.localStyles.addReaction}
+              >
+                <Icon
+                  name="plus"
+                  type="entypo"
+                  size={fontScale * 16}
+                  color={colors.primaryDark}
+                />
+              </TouchableOpacity>
+            )}
+          </View>
+        </View>
+      </View>
+    );
+  }
+  localStyles = StyleSheet.create({
+    containerView: {
+      width:
+        this.props.type == 'assignment'
+          ? (2.4 * screenWidth) / 3
+          : (2 * screenWidth) / 3,
+      height:
+        this.props.type == 'assignment' ? screenHeight / 3 : screenHeight / 7,
+      alignSelf:
+        this.props.madeByUser == this.props.currentUser.ID
+          ? 'flex-end'
+          : 'flex-start',
+      flexDirection:
+        this.props.madeByUser == this.props.currentUser.ID
+          ? 'row-reverse'
+          : 'row',
+      marginTop: this.props.number == 0 ? screenHeight / 40 : 0,
+      marginBottom: screenHeight / 40
+    },
+    assignmentContainer: {
+      alignSelf: 'center',
+      flex: 3,
+      overflow: 'hidden',
+      borderColor: colors.lightBrown,
+      borderWidth: 2,
+      marginTop: screenHeight / 163.2,
+      width: screenWidth / 1.6
+    },
+    spinnerContainerStyle: {
+      flex: 1,
+      alignItems: 'center',
+      justifyContent: 'center'
+    },
+    contentText: {
+      color: colors.lightBrown,
+      fontWeight: 'bold',
+      fontSize: fontScale * 15
+    },
+    userImage: {
+      width: screenScale * 18,
+      height: screenScale * 18,
+      marginLeft:
+        this.props.madeByUser == this.props.currentUser.ID
+          ? 0
+          : screenWidth / 25,
+      marginRight:
+        this.props.madeByUser == this.props.currentUser.ID
+          ? screenWidth / 25
+          : 0,
+      resizeMode: 'contain'
+    },
+    newAssignmentText: {
+      fontSize: fontScale * 16,
+      color: colors.lightBrown,
+      fontWeight: 'bold'
+    },
+    newAssignmentTextContainer: {
+      flexDirection: 'row'
+    },
+    addReaction: {
+      alignItems: 'center',
+      justifyContent: 'center',
+      width: screenScale * 8,
+      height: screenScale * 8,
+      borderWidth: 1,
+      backgroundColor: colors.primaryLight,
+      borderColor: colors.primaryLight,
+      borderRadius: (screenScale * 8) / 2,
+      alignSelf: 'flex-end',
+      position: 'relative',
+      bottom: screenScale * 4,
+      left: screenScale * 4
+    },
+    Reaction: {
+      alignItems: 'center',
+      justifyContent: 'space-around',
+      flexDirection: 'row',
+      width: screenScale * 16,
+      height: screenScale * 8,
+      borderWidth: 1,
+      backgroundColor: colors.primaryLight,
+      borderColor: colors.primaryLight,
+      borderRadius: (screenScale * 8) / 2,
+      alignSelf: 'flex-end',
+      position: 'relative',
+      bottom: screenScale * 4,
+      left: screenScale * 4,
+      marginRight: screenWidth / 86
+    },
+    contentContainerView: {
+      borderWidth: 2,
+      marginTop: 0,
+      flex: 5,
+      paddingBottom:
+        this.props.type === 'assignment' ? screenHeight / 163.5 : 0,
+      alignItems: 'center',
+      justifyContent: 'center',
+      borderColor: colors.lightBrown,
+      backgroundColor: colors.white,
+    }
+  });
 }
