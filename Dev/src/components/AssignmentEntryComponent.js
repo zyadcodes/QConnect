98cad74c261230c0852043eb1b301e427175696e
Elyasse <elyassedev@outlook.com>
2020-10-24 12:52:00 -0700

    Install new Modal component + fixed StudMainScr formatting

--- Dev/src/components/AssignmentEntryComponent.js
+++ Dev/src/components/AssignmentEntryComponent.js
@@ -1,6 +1,7 @@
 import React from "react";
 import { View, StyleSheet } from "react-native";
 import { Overlay } from "react-native-elements";
+import Modal, { ModalContent } from "react-native-modals";
 import QcActionButton from "components/QcActionButton";
 import strings from "config/strings";
 import surahNames from "config/surahNames";
@@ -25,47 +26,45 @@ export default class AssignmentEntryComponent extends React.Component {
   render() {
     return (
       <View>
-        <Overlay
-          isVisible={this.props.visible}
-          onBackdropPress={() => this.props.onCancel()}
-          overlayStyle={{
-            width: screenWidth * 0.9,
-            height: screenHeight * 0.85
-          }}
+        <Modal
+          visible={this.props.visible}
+          onTouchOutside={() => this.props.onCancel()}
         >
-          <View style={styles.container}>
-            <View style={styles.spacer} />
-            <InputAutoSuggest
-              staticData={surahNames}
-              onTextChanged={this.onTextChange.bind(this)}
-              onSurahTap={(name, ename, id) =>
-                this.props.onSubmit({ name, ename, id })
-              }
-              assignment={
-                this.props.assignment === strings.None
-                  ? ""
-                  : this.props.assignment
-              }
-              inputStyle={fontStyles.mainTextStyleBlack}
-              itemTextStyle={fontStyles.mainTextStyleDarkGrey}
-            />
+          <ModalContent>
+            <View style={styles.container}>
+              <View style={styles.spacer} />
+              <InputAutoSuggest
+                staticData={surahNames}
+                onTextChanged={this.onTextChange.bind(this)}
+                onSurahTap={(name, ename, id) =>
+                  this.props.onSubmit({ name, ename, id })
+                }
+                assignment={
+                  this.props.assignment === strings.None
+                    ? ""
+                    : this.props.assignment
+                }
+                inputStyle={fontStyles.mainTextStyleBlack}
+                itemTextStyle={fontStyles.mainTextStyleDarkGrey}
+              />
 
-            <View style={styles.footer}>
-              <QcActionButton
-                text={strings.Go}
-                onPress={() => {
-                  this.props.assignmentType
-                    ? this.props.onSubmit(this.state.input, this.state.type)
-                    : this.props.onSubmit(this.state.input);
-                }}
-              />
-              <QcActionButton
-                text={strings.Cancel}
-                onPress={() => this.props.onCancel()}
-              />
+              <View style={styles.footer}>
+                <QcActionButton
+                  text={strings.Go}
+                  onPress={() => {
+                    this.props.assignmentType
+                      ? this.props.onSubmit(this.state.input, this.state.type)
+                      : this.props.onSubmit(this.state.input);
+                  }}
+                />
+                <QcActionButton
+                  text={strings.Cancel}
+                  onPress={() => this.props.onCancel()}
+                />
+              </View>
             </View>
-          </View>
-        </Overlay>
+          </ModalContent>
+        </Modal>
       </View>
     );
   }
